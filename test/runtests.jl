67a22e857b5487bd7006ea2f9c09573dc13ce6f3
Curtis Vogt <curtis.vogt@gmail.com>
2021-04-09 15:35:36 -0500

    Add duplicate stack testcase

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,20 @@
-using Test
-using K8sClusterManagers
 using Distributed
+using K8sClusterManagers
+using Swagger
+using Test
 
+
+@testset "K8sClusterManagers" begin
+    @testset "addprocs_pod" begin
+        @testset "pods not found" begin
+            withenv("HOSTNAME" => "localhost") do
+                try
+                    K8sClusterManagers.addprocs_pod(1)
+                catch ex
+                    @test ex isa Swagger.ApiException
+                    @test length(Base.catch_stack()) == 2  # Ideally would be 1...
+                end
+            end
+        end
+    end
+end
