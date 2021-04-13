4502ad46e1a0cd2ddf5704124d53c1ec1feb0945
Curtis Vogt <curtis.vogt@gmail.com>
2021-04-13 14:56:28 -0500

    Note about KUBECTL_PROXY_PORT

--- src/K8sClusterManagers.jl
+++ src/K8sClusterManagers.jl
@@ -18,6 +18,8 @@ export launch, manage, kill
 const kubectl_proxy_process = Ref{Base.Process}()
 
 function restart_kubectl_proxy()
+    # Note: "KUBECTL_PROXY_PORT" is a made up environmental variable and is not supported by
+    # `kubectl proxy`.
     port = get(ENV, "KUBECTL_PROXY_PORT", 8001)
     if isassigned(kubectl_proxy_process)
         kill(kubectl_proxy_process[])
