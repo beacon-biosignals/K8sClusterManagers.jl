d0d47be11c5b8139707d324b631d67cb67085fac
Curtis Vogt <curtis.vogt@gmail.com>
2021-04-19 19:49:11 -0500

    Add comments for `restart_kubectl_proxy` (#21)

--- src/K8sClusterManagers.jl
+++ src/K8sClusterManagers.jl
@@ -18,6 +18,9 @@ export launch, manage, kill
 const kubectl_proxy_process = Ref{Base.Process}()
 
 function restart_kubectl_proxy()
+    # Note: "KUBECTL_PROXY_PORT" is a made up environmental variable and is not supported by
+    # `kubectl proxy`. The default port (8001) is what `kubectl proxy` uses when `--port` is
+    # not specified.
     port = get(ENV, "KUBECTL_PROXY_PORT", 8001)
     if isassigned(kubectl_proxy_process)
         kill(kubectl_proxy_process[])
@@ -33,6 +36,7 @@ function __init__()
               "https://github.com/JuliaBinaryWrappers/kubectl_jll.jl#platforms")
     end
 
+    # Kuber.jl expects that Kubernetes API server is available via: http://localhost:8001
     restart_kubectl_proxy()
 end
 
