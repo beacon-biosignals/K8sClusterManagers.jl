263e04c6c783cdf62876bb00ba06c699964a040c
Curtis Vogt <curtis.vogt@gmail.com>
2023-04-10 14:06:13 -0500

    Add default test for K8sClusterManager

--- test/native_driver.jl
+++ test/native_driver.jl
@@ -1,4 +1,15 @@
 @testset "K8sClusterManager" begin
+    @testset "basic" begin
+        mgr = K8sClusterManager(1; image="julia:1")
+        @test mgr.np == 1
+        @test mgr.pod_name == gethostname()
+        @test mgr.image == "julia:1"
+        @test mgr.cpu == string(K8sClusterManagers.DEFAULT_WORKER_CPU)
+        @test mgr.memory == K8sClusterManagers.DEFAULT_WORKER_MEMORY
+        @test mgr.pending_timeout == 180
+        @test mgr.configure === identity
+    end
+
     @testset "pods not found" begin
         try
             K8sClusterManager(1)
